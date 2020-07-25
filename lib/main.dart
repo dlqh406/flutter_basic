3db7af2f4677b10175f4957313517c68851d9a23
Lee bosung <dlqh406@gmail.com>
2020-07-25 14:46:01 +0900

    StatefulWidget 아래 코드에 simplify 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,45 @@
 import 'package:flutter/material.dart';
 
+//빌드가 시작됨
 void main() {
   runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
-  // statelessWidget : 상태를 가지를 수 없는 위젯
-  // This widget is the root of your application.
+  // statelessWidget : 상태를 가지를 수 없는 위젯, 딱 멈춰있는 위젯
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
-        home: Hellopage("hello bosung"));
+        home: Hellopage());
   }
 }
+
 // stfull을 입력하면 자동 완성됨
 // 화면을 아래처럼 하나씩 만든다, 그리고 위 myapp에서 불러온다
 class Hellopage extends StatefulWidget {
- final String title;
- Hellopage(this.title);
-
   @override
   _HellopageState createState() => _HellopageState();
 }
 
 class _HellopageState extends State<Hellopage> {
+  String _title = "hello1";
   String _message = "yet pushed";
   int _count = 0;
   // _variable : 여기 안에서만 사용하는 변수
+  // build 안에서 사용할 변수를 여기에 선언한다
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child : Icon(Icons.add),
         onPressed: _chageMessage),
 
-      appBar: AppBar(title : Text(widget.title),),
+      appBar: AppBar(title : Text(_title),),
         body: Center(
             child:Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -51,9 +52,6 @@ class _HellopageState extends State<Hellopage> {
                   ],
                 ),
             )
-
-
-
     );
   }
 
