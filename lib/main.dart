9a45216a8180cee62c62d34857811aeb9c21d1e2
Lee bosung <dlqh406@gmail.com>
2020-07-25 09:05:08 +0900

    StatefulWidget 생성

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
+  // statelessWidget : 상태를 가지를 수 없는 위젯
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -12,10 +13,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar :  AppBar( title: Text("hello world"),),
-          body :  Text('hello world', style: TextStyle(fontSize: 30),)),
+        home: Hellopage("hello bosung"));
+  }
+}
+// stfull을 입력하면 자동 완성됨
+// 화면을 아래처럼 하나씩 만든다, 그리고 위 myapp에서 불러온다
+class Hellopage extends StatefulWidget {
+ final String title;
+ Hellopage(this.title);
 
+  @override
+  _HellopageState createState() => _HellopageState();
+}
+
+class _HellopageState extends State<Hellopage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title : Text(widget.title),),
+        body: Text(widget.title,style:TextStyle(fontSize: 30))
     );
   }
 }
